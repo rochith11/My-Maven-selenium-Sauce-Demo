54c89ca842425f60e361803c3b685af292c10f34
rochith11 <rochith.s.phutane10@gmail.com>
2026-04-13 20:00:42 +0530

    Added imports to App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,4 +1,8 @@
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class App {
     public static void main(String[] args) throws InterruptedException {
